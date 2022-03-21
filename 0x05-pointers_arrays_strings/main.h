14e0aa4d12393553716731888969dfbfe9e47f91
alayonu <olaniyiesther17@yahoo.com>
2022-03-21 11:22:59 -0700

    my commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _main_h_
+#define _main_h_
 
-/**
- * File: holberton.h
- * Auth: Olaniyi Esther Adufe
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
-
-#include <stdlib.h>
-
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
+int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
@@ -21,5 +11,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s);                                        
+
 #endif
