564bf4474917577105c1ccb8dc5b0b283f60b989
Olaniyi Esther Adufe <99925014+Alayonu@users.noreply.github.com>
2022-04-11 20:42:29 +0100

    Update dog.h

--- 0x0E-Structures_typedef/dog.h
+++ 0x0E-Structures_typedef/dog.h
@@ -2,6 +2,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * File: dog.h
+ * Auth: Olaniyi Esther Adufe
+ * Desc: Header file that defines a new type struct dog.
+ */
+
 /**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog.
